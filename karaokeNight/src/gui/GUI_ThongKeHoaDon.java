3fa49687cff04a2ca3728d5546ccd4db0d4adb6d
Hoafng <hoangghcc@gmail.com>
2022-12-05 17:59:45 +0700

    Update GUI_ThongKeHoaDon.java

--- karaokeNight/src/gui/GUI_ThongKeHoaDon.java
+++ karaokeNight/src/gui/GUI_ThongKeHoaDon.java
@@ -1,18 +1,23 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.sql.SQLException;
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -22,23 +27,30 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.UIManager;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 import com.toedter.calendar.JDateChooser;
 
-import dao.Dao_HoaDon;
-import entity.CTHoaDonThuePhong;
+import connect.ConnectDB;
+import dao.Dao_ThongKeDoanhThu;
+import dao.Dao_ThongKeDoanhThu;
+import entity.DichVu;
 import entity.HoaDonThuePhong;
 import entity.TaiKhoan;
 
-public class GUI_ThongKeHoaDon extends JFrame {
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
+import javax.swing.RowFilter;
+
 
 	private JPanel contentPane;
 	private DefaultTableModel modelTkHoaDon;
@@ -48,9 +60,14 @@ public class GUI_ThongKeHoaDon extends JFrame {
 	private JTextField txtTuNgay;
 	private JTextField txtDenNgay;
 	private TaiKhoan tk;
-	private Dao_HoaDon dao_HoaDon = new Dao_HoaDon();
-	private JLabel lblKqHoaDon;
-	private JLabel lblKqTienCacHoaDon;
+	private Dao_ThongKeDoanhThu dao_tkhd;
+	private Dao_ThongKeDoanhThu dao_hd;
+	private boolean checkDateChooser = false, checkDateChooser_1 = false;
+	private DefaultTableModel TempModelTkDoanhThu;
+	JLabel lblTongTienDoanhThu;
+	private JLabel lblKqDoanhThu;
+	private JLabel lblKqHdCaoNhat;
+	private JLabel lblKqHdThapNhat;
 
 	/**
 	 * Launch the application.
@@ -59,7 +76,16 @@ public class GUI_ThongKeHoaDon extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public GUI_ThongKeHoaDon(TaiKhoan taiKhoan) {
+
+		try {
+			ConnectDB.getInstance().connect();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		dao_hd = new Dao_ThongKeDoanhThu();
+		dao_tkhd = new Dao_ThongKeDoanhThu();
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100,1480,780);
 		setLocationRelativeTo(null);
@@ -200,11 +226,11 @@ public class GUI_ThongKeHoaDon extends JFrame {
 		pnTop.setBackground(new Color(101, 186, 118));
 		pnTop.setBounds(0, 10, 1466, 52);
 		contentPane.add(pnTop);
-		
-		JLabel lblThongKeHoaDon = new JLabel("Thống kê hóa đơn");
+
+		JLabel lblThongKeHoaDon = new JLabel("Thống kê doanh thu");
 		lblThongKeHoaDon.setFont(new Font("Times New Roman", Font.BOLD, 30));
 		pnTop.add(lblThongKeHoaDon);
-		
+
 
 		JPanel pnLeft = new JPanel();
 		pnLeft.setBackground(new Color(101, 186, 118));
@@ -212,55 +238,34 @@ public class GUI_ThongKeHoaDon extends JFrame {
 		contentPane.add(pnLeft);
 		pnLeft.setBorder(new TitledBorder("Nhập vào để thống kê"));
 		pnLeft.setLayout(null);
-		
+
 		JLabel lblTuNgay = new JLabel("Từ ngày :");
 		lblTuNgay.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		lblTuNgay.setBounds(60, 65, 92, 34);
 		dateChooser = new JDateChooser();
-		dateChooser.getDateEditor().addPropertyChangeListener(
-		    new PropertyChangeListener() {
-		        @Override
-		        public void propertyChange(PropertyChangeEvent e) {
-		        	SimpleDateFormat sf = new SimpleDateFormat("dd/MM/yyyy");																																																																																																																																																												
-		            if ("date".equals(e.getPropertyName())) {
-		            	String a = sf.format(e.getNewValue());		   
-		            	txtTuNgay.setText(a + "");
-		            }
-		        }
-		    });
+
 		dateChooser.getCalendarButton().setBounds(169, 0, 31, 30);
 		dateChooser.setBounds(151, 69, 200, 30);
 		pnLeft.add(dateChooser);
 		dateChooser.setLayout(null);
-		
+
 		txtTuNgay = new JTextField();
 		txtTuNgay.setFont(new Font("Times New Roman", Font.PLAIN, 16));
 		txtTuNgay.setBounds(0, 0, 170, 30);
 		dateChooser.add(txtTuNgay);
 		txtTuNgay.setColumns(10);
 		pnLeft.add(lblTuNgay);
-		
-		
+
+
 		JLabel lblDenNgay = new JLabel("Đến ngày:");
 		lblDenNgay.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		lblDenNgay.setBounds(60, 142, 84, 30);
 		dateChooser_1 = new JDateChooser();
-		dateChooser_1.getDateEditor().addPropertyChangeListener(
-		    new PropertyChangeListener() {
-		        @Override
-		        public void propertyChange(PropertyChangeEvent e) {
-		        	SimpleDateFormat sf = new SimpleDateFormat("dd/MM/yyyy");																																																																																																																																																												
-		            if ("date".equals(e.getPropertyName())) {
-		            	String a = sf.format(e.getNewValue());		   
-		            	txtDenNgay.setText(a + "");
-		            }
-		        }
-		    });
 		dateChooser_1.getCalendarButton().setBounds(170, 0, 30, 30);
 		dateChooser_1.setBounds(151, 142, 200, 30);
 		pnLeft.add(dateChooser_1);
 		dateChooser_1.setLayout(null);
-		
+
 		txtDenNgay = new JTextField();
 		txtDenNgay.setFont(new Font("Times New Roman", Font.PLAIN, 16));
 		txtDenNgay.setColumns(10);
@@ -268,25 +273,25 @@ public class GUI_ThongKeHoaDon extends JFrame {
 		dateChooser_1.add(txtDenNgay);
 		pnLeft.add(lblDenNgay);
 
-	
-		
-		
-		ImageIcon iconLamMoi = new ImageIcon("image//iconButton//Refresh.png");
+
+
+
+		ImageIcon iconLamMoi = new ImageIcon("IMG//iconButton//Refresh.png");
 		JButton btnLamMoiHoaDon = new JButton("Làm mới", iconLamMoi);
 		btnLamMoiHoaDon.setBackground(new Color(255, 255, 140));
 		btnLamMoiHoaDon.setFont(new Font("Times New Roman", Font.PLAIN, 20));
 		btnLamMoiHoaDon.setBounds(60, 371, 291, 33);
 		pnLeft.add(btnLamMoiHoaDon);
-		
-		ImageIcon iconThongKe = new ImageIcon("image//iconButton//statistical.png");
+
+		ImageIcon iconThongKe = new ImageIcon("IMG//iconButton//statistical.png");
 		JButton btnThongKeHoaDon = new JButton("Thống kê", iconThongKe);
 		btnThongKeHoaDon.setBackground(new Color(255, 255, 140));
 		btnThongKeHoaDon.setForeground(new Color(0, 0, 0));
 		btnThongKeHoaDon.setFont(new Font("Times New Roman", Font.PLAIN, 20));
 		btnThongKeHoaDon.setBounds(60, 301, 291, 33);
 		pnLeft.add(btnThongKeHoaDon);
-		
-		JButton btnInThongKeHoaDon = new JButton("In thống kê", new ImageIcon("image//iconButton//print.png"));
+
+		JButton btnInThongKeHoaDon = new JButton("Biểu đồ thống kê", new ImageIcon("IMG//iconButton//print.png"));
 		btnInThongKeHoaDon.setForeground(Color.BLACK);
 		Border roundedBorder = new LineBorder(new Color(210,210,210), 1, true);
 		btnInThongKeHoaDon.setBorder(new LineBorder(new Color(210, 210, 210), 1, true));
@@ -294,51 +299,181 @@ public class GUI_ThongKeHoaDon extends JFrame {
 		btnInThongKeHoaDon.setBackground(new Color(255, 255, 140));
 		btnInThongKeHoaDon.setBounds(60, 445, 291, 33);
 		pnLeft.add(btnInThongKeHoaDon);
-		
 
-	
+
+
 		JPanel pnCenter = new JPanel();
 		pnCenter.setBackground(new Color(101, 186, 118));
 		pnCenter.setBounds(433, 66, 1033, 673);
 		contentPane.add(pnCenter);
-		String[] cols = { "Mã hóa đơn", "Nhân viên lập hóa đơn", "Ngày lập hóa đơn", "Phòng", "Thành tiền"};
+		String[] cols = { "Mã hóa đơn", "Nhân viên lập hóa đơn", "Ngày lập hóa đơn", "Tên khách hàng","Tiền nhận của khách", "Tiên dư của khách", "Tổng tiền hóa đơn"};
 		modelTkHoaDon = new DefaultTableModel(cols, 0);
 		pnCenter.setLayout(null);
 		tblTkHoaDon = new JTable(modelTkHoaDon);
 		JScrollPane scrtbl = new JScrollPane(tblTkHoaDon, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		pnCenter.setBorder(BorderFactory.createTitledBorder("Danh sách hóa đơn"));
-		scrtbl.setBounds(10, 25, 1013, 554);
+		scrtbl.setBounds(10, 25, 1013, 454);
 		pnCenter.add(scrtbl);
+
+		TempModelTkDoanhThu = new DefaultTableModel(cols, 0);
+
+		JLabel lblDoanhThu = new JLabel("Tổng doanh thu:");
+		lblDoanhThu.setForeground(new Color(255, 0, 0));
+		lblDoanhThu.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblDoanhThu.setBounds(790, 520, 110, 26);
+		pnCenter.add(lblDoanhThu);
+
+		lblKqDoanhThu = new JLabel("0 VNĐ");
+		lblKqDoanhThu.setForeground(new Color(255, 0, 0));
+		lblKqDoanhThu.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblKqDoanhThu.setBounds(905, 523, 95, 21);
+		pnCenter.add(lblKqDoanhThu);
+
+		lblKqHdCaoNhat = new JLabel("");
+		lblKqHdCaoNhat.setForeground(new Color(255, 0, 0));
+		lblKqHdCaoNhat.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblKqHdCaoNhat.setBounds(144, 523, 95, 21);
+		pnCenter.add(lblKqHdCaoNhat);
+
+		JLabel lblHoaDonCaoNhat = new JLabel("Hóa đơn cao nhất:");
+		lblHoaDonCaoNhat.setForeground(new Color(255, 0, 0));
+		lblHoaDonCaoNhat.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblHoaDonCaoNhat.setBounds(13, 520, 121, 26);
+		pnCenter.add(lblHoaDonCaoNhat);
+
+		JLabel lblHoaDonThapNhat = new JLabel("Hóa đơn thấp nhất:");
+		lblHoaDonThapNhat.setForeground(Color.RED);
+		lblHoaDonThapNhat.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblHoaDonThapNhat.setBounds(10, 556, 121, 26);
+		pnCenter.add(lblHoaDonThapNhat);
+
+		lblKqHdThapNhat = new JLabel("0VNĐ");
+		lblKqHdThapNhat.setForeground(Color.RED);
+		lblKqHdThapNhat.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblKqHdThapNhat.setBounds(144, 559, 95, 21);
+		pnCenter.add(lblKqHdThapNhat);
 		
-		JLabel lblTongHoaDon = new JLabel("Tổng số hóa đơn : ");
-		lblTongHoaDon.setHorizontalAlignment(SwingConstants.LEFT);
-		lblTongHoaDon.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-		lblTongHoaDon.setBounds(816, 589, 118, 26);
-		pnCenter.add(lblTongHoaDon);
-		
-		lblKqHoaDon = new JLabel("");
-		lblKqHoaDon.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-		lblKqHoaDon.setBounds(944, 592, 70, 21);
-		pnCenter.add(lblKqHoaDon);
-		
-		JLabel lblTongTienCacHoaDon = new JLabel("Tổng tiền các hóa đơn:");
-		lblTongTienCacHoaDon.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-		lblTongTienCacHoaDon.setBounds(10, 589, 150, 26);
-		pnCenter.add(lblTongTienCacHoaDon);
-		
-		lblKqTienCacHoaDon = new JLabel("0 VNĐ");
-		lblKqTienCacHoaDon.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-		lblKqTienCacHoaDon.setBounds(170, 592, 143, 21);
-		pnCenter.add(lblKqTienCacHoaDon);
-		DocDuLieuDatabaseVaoTable();
+
+		dateChooser.getDateEditor().addPropertyChangeListener(
+				new PropertyChangeListener() {
+					@Override
+					public void propertyChange(PropertyChangeEvent e) {
+						SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");																																																																																																																																																												
+						if ("date".equals(e.getPropertyName())) {
+							String a = sf.format(e.getNewValue());		   
+							txtTuNgay.setText(a + "");
+							checkDateChooser = true;
+							if(checkDateChooser && checkDateChooser_1) {
+								ArrayList<HoaDonThuePhong> listHD = dao_tkhd.getHoaDonDalap(txtTuNgay.getText().toString(), txtDenNgay.getText().toString());
+								DocDuLieuDatabaseVaoTable(listHD, TempModelTkDoanhThu);
+								tblTkHoaDon.setModel(TempModelTkDoanhThu);
+								TempModelTkDoanhThu = new DefaultTableModel(cols, 0);
+							}
+						}
+					}
+				});
+
+		dateChooser_1.getDateEditor().addPropertyChangeListener(
+				new PropertyChangeListener() {
+					@Override
+					public void propertyChange(PropertyChangeEvent e) {
+						SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");																																																																																																																																																												
+						if ("date".equals(e.getPropertyName())) {
+							String a = sf.format(e.getNewValue());		   
+							txtDenNgay.setText(a + "");
+							checkDateChooser_1 = true;
+							if(checkDateChooser && checkDateChooser_1) {
+								ArrayList<HoaDonThuePhong> listHD = dao_tkhd.getHoaDonDalap(txtTuNgay.getText().toString(), txtDenNgay.getText().toString());
+								DocDuLieuDatabaseVaoTable(listHD, TempModelTkDoanhThu);
+								tblTkHoaDon.setModel(TempModelTkDoanhThu);
+								TempModelTkDoanhThu = new DefaultTableModel(cols, 0);
+							}
+							else if(txtTuNgay.getText().toString() != null && lblDenNgay.getText().toString() != null) {
+								TableRowSorter<TableModel> rowSorter
+								= new TableRowSorter<>(tblTkHoaDon.getModel()) ;
+								tblTkHoaDon.setRowSorter(rowSorter);
+								txtTuNgay.getDocument().addDocumentListener(new DocumentListener() {
+									@Override
+									public void insertUpdate(DocumentEvent e) {
+										String text = txtTuNgay.getText();
+										if (text.trim().length() == 0) {
+											rowSorter.setRowFilter(null);
+										} else {
+											rowSorter.setRowFilter(RowFilter.regexFilter("(?i)" + text));
+										}
+									}
+
+									@Override
+									public void removeUpdate(DocumentEvent e) {
+										String text = txtTuNgay.getText();
+
+										if (text.trim().length() == 0) {
+											rowSorter.setRowFilter(null);
+										} else {
+											rowSorter.setRowFilter(RowFilter.regexFilter("(?i)" + text));
+										}
+									}
+
+									@Override
+									public void changedUpdate(DocumentEvent e) {
+										throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+									}
+								});
+							}
+						}
+					}
+
+				});
+		ArrayList<HoaDonThuePhong> listHD = dao_tkhd.getAllHoaDon();
+		DocDuLieuDatabaseVaoTable(listHD, modelTkHoaDon);
 	}
-	public void DocDuLieuDatabaseVaoTable() {
-		ArrayList<HoaDonThuePhong> listHoaDon = dao_HoaDon.getAllHoaDon();
-		for (HoaDonThuePhong hd : listHoaDon) {
-			modelTkHoaDon.addRow(new Object[] {hd.getMaHoaDon(),hd.getMaNhanVien().getMaNhanVien(),hd.getNgayLap(),hd.getMaPhong().getMaPhong()});
+	public void DocDuLieuDatabaseVaoTable(ArrayList<HoaDonThuePhong> listHD, DefaultTableModel modelTkHoaDon) {
+		long sum = 0;
+		long minDT = 0;
+		long maxDT = 0;
+
+		for (HoaDonThuePhong hd : listHD) {
+
+			long tt = tongTien(hd.getGioVaoPhong(), hd.getGioRaPhong(),hd.getMaPhong().getGiaPhong(), 
+					hd.getCthdt().getSoLuongDichVu(), hd.getCthdt().getMaDichVu().getGiaDichVu());
+
+			sum = sum + tt;
+			
+			if(tt >= maxDT) {
+				maxDT = tt;
+			}
+
+			long tdck = tienDuCuaKhach(hd.getGioVaoPhong(), hd.getGioRaPhong(),hd.getMaPhong().getGiaPhong(), 
+					hd.getCthdt().getSoLuongDichVu(),  hd.getCthdt().getMaDichVu().getGiaDichVu(), hd.getCthdt().getTienNhanCuaKhach());
+			
+			if(tt<=minDT) {
+				minDT = tt;
+			}
+			modelTkHoaDon.addRow(new Object[] {
+					hd.getMaHoaDon(),hd.getNhanVien().getTenNhanVien(), hd.getNgayLap(), hd.getKhachhang().getTenKhachHang(),
+					formatNumberForMoney(hd.getCthdt().getTienNhanCuaKhach()),formatNumberForMoney(tdck), formatNumberForMoney(tt)
+			});
 		}
-		int sum = listHoaDon.size();
-		lblKqHoaDon.setText(""+ sum);
+		lblKqDoanhThu.setText(sum + " VNĐ");
+		lblKqHdCaoNhat.setText(maxDT + " VNĐ");
+		lblKqHdThapNhat.setText(minDT + " VNĐ");
+	}
+	public long tongTien(LocalDateTime gioVaoPhong, LocalDateTime gioRaPhong, double giaPhong, int soLuongDichVu, double giaDichVu) {
+
+		double tongTien = (gioRaPhong.getHour() - gioVaoPhong.getHour())* giaPhong + soLuongDichVu* giaDichVu;
+		return (long) tongTien;
+	}
+	public long tienDuCuaKhach(LocalDateTime gioVaoPhong, LocalDateTime gioRaPhong, double giaPhong, int soLuongDichVu, double giaDichVu, double tienNhanCuaKhach) {
+		double tienDuCuaKhach = tienNhanCuaKhach - ((gioRaPhong.getHour() - gioVaoPhong.getHour())* giaPhong + soLuongDichVu* giaDichVu);
+		return (long) tienDuCuaKhach;
+
+	}
+	private String formatNumberForMoney(double money) {
+		Locale localeVN = new Locale("vi", "VN");
+		NumberFormat currencyVN = NumberFormat.getCurrencyInstance(localeVN);
+		String str1 = currencyVN.format(Math.round(money));
+		str1 = str1.substring(0,str1.length() - 2);
+		return str1 + " VNĐ";
 	}
 }
